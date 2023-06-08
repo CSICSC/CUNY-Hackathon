ff6057a81c6a90d13ad4e8da85531fdc4e8da13e
prince-ao <prince.addo@cix.csi.cuny.edu>
2023-06-08 10:31:07 -0400

    header framework complete

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,60 @@
+<nav>
+    <div class="site-id">
+        <slot>
+            <h1>Site ID goes here</h1>
+        </slot>
+    </div>
+    <div class="nav-links">
+        <ul>
+            <li>tracks</li>
+            <li>sponsors</li>
+            <li>faq</li>
+        </ul>
+    </div>
+</nav>
+
+<style lang="scss">
+    nav {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        position: fixed; /* position varies */
+        border-bottom: 1px solid black;
+        width: 100%;
+        height: 5vh;
+        background-color: #F5E5B0;
+
+        .site-id {
+            display: flex;
+            margin-left: 10%;
+            flex-grow: 1;
+            align-items: center;
+        }
+
+        .nav-links {
+            flex-grow: 1;
+            display: flex;
+            justify-content: flex-end;
+            margin-right: 10%;
+            ul {
+                display: flex;
+                justify-content: space-around;
+                width: 60%;
+
+                li {
+                    font-family: Arial, Helvetica, sans-serif;
+                    font-size: 20px;
+                    font-weight: bold;
+                    list-style-type: none;
+                    text-transform: uppercase;
+                    
+                    &:hover {
+                        cursor: pointer;
+                        color: #233dff;
+                    }
+                }
+            }
+        }
+    }
+
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,36 @@
-<script></script>
+<script>
+    import '$styles/global.scss'
+    import Header from '$components/Header.svelte'
+    import logo2 from '$images/logo2.png'
+</script>
 
-<h1>Hello CUNY Hackthon</h1>
+<Header>
+    <div class="header-container">
+        <img class="logo" src={logo2} alt="logo">
+        <h1 class="site-id">CUNY Hacks <span>2023</span></h1>
+    </div>
+</Header>
 
-<style></style>
+<style lang="scss">
+    .header-container {
+        display: inherit;
+        align-items: inherit;
+
+        img {
+            width: 50px;
+        }
+
+        h1 {
+            font-family: 'Gasoek One';
+            font-size: 30px;
+            span {
+                color: #233dff;
+            }
+        }
+
+        &:hover {
+            cursor: pointer;
+        }
+    }
+
+</style>
