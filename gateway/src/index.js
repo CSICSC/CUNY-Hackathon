858520625c6343b6f0fbf3c9a03a92644a0d38cb
prince-ao <prince.addo@cix.csi.cuny.edu>
2023-06-24 18:22:16 -0400

    proxy server fix

--- gateway/src/index.js
+++ gateway/src/index.js
@@ -15,9 +15,14 @@ let currentServer = 0;
 
 gateway.use(cors());
 gateway.use((req, res) => {
-    const targetServer = servers[currentServer];
-    currentServer = (currentServer + 1)&servers.length
-    console.log(`LOG(proxy): routing to ${currentServer}`)
+
+    if (!req.session.currentServer) {
+        req.session.currentServer = Math.floor(Math.random() * servers.length);
+    }
+
+    const targetServer = servers[req.session.currentServer];
+    console.log(`LOG(proxy): routing to ${targetServer}`)
+
     proxy.web(req, res, {target: `http://${targetServer.host}:${targetServer.port}`});
 })
 
